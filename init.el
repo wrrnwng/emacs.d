67d1f9ef6acbd6e9afbf3b8f5b4df04877179fdc
Warren Wong <me@warrenwong.org>
2022-06-01 23:07:23 +0800

    update init.el

--- init.el
+++ init.el
@@ -11,7 +11,7 @@
 
 (set-face-attribute 'default nil :font "Fira Code Retina" :height 120)
 
-(load-theme 'tango-dark)
+(load-theme 'wombat)
 
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
@@ -33,6 +33,15 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
+(column-number-mode)
+(global-display-line-numbers-mode t)
+
+(dolist (mode '(org-mode-hook
+		term-mode-hook
+		shell-mode-hook
+		eshell-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
+
 (use-package command-log-mode)
 
 (use-package ivy
@@ -50,15 +59,35 @@
 	:map ivy-reverse-i-search-map
 	("C-k" . ivy-previous-line)
 	("C-d" . ivy-reverse-i-search-kill))
-  :config
+  :init
   (ivy-mode 1))
+
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+(use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode
+  :config
+  (setq which-key-idle-delay 1))
+
+(use-package counsel
+  :bind (("M-x" . counsel-M-x)
+	 ("C-x b" . counsel-ibuffer)
+	 ("C-x C-f" . counsel-find-file)
+	 :map minibuffer-local-map
+	 ("C-r" . 'counsel-minibuffer-history)))
+
+(use-package ivy-rich
+  :init
+  (ivy-rich-mode 1))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(ivy command-log-mode use-package)))
+ '(package-selected-packages '(counsel ivy command-log-mode use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
